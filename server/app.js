a9e4c205f56e0e2167b0d96386bcfdb8f7d535d9
samuelzega <samuel.zegaa@gmail.com>
2020-02-20 14:42:30 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv')
+var express = require('express')
+var cors = require('cors')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+
+var app = express()
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+
+module.exports = app
