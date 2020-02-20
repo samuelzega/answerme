8362431eb72e636e7f4d25d63adc9769fafdcc4c
purandrar <andra_pur@yahoo.com>
2020-02-20 21:39:36 +0700

    socket sudah bisa berkomunikasih dari server ke client

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import io from 'socket.io-client';
 Vue.config.productionTip = false
-
+Vue.prototype.$socket = io.connect(`http://localhost:3000`)
 new Vue({
   router,
   store,
---
+++ server/controllers/rooms.js
@@ -0,0 +1,60 @@
+const { Room } = require("../models")
+
+class RoomController {
+    static getAll(req, res, next) {
+        Room.findAll()
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch(err => {
+                res.status(404).json(err.message);
+            });
+    }
+    static findOne(payload) {
+        Room.findOne({
+            where: {
+                name: payload.name
+            }
+        })
+            .then(result => {
+                if (result !== null) {
+                    return result
+                }
+            })
+            .catch(error => {
+                return { message: "room not found" }
+            })
+    }
+    static createOne(payload) {
+        let objInput = {
+            name: payload.name,
+            max_user: 4
+        }
+        Room.create(objInput)
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    static deleteOne(req, res, next) {
+        Room.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(result => {
+                if (result === 0) {
+                    res.status(404).json({ message: "data not found" });
+                } else {
+                    res.status(200).json(result);
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(400).json(error);
+            });
+    }
+}
+module.exports = RoomController
