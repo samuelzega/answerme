15a63f27e6c0bfe15c6af043a0aa8f06c1804f76
samuelzega <samuel.zegaa@gmail.com>
2020-02-21 12:26:47 +0700

    samuelzega

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div id="nav">
+    <!-- <div id="nav">
       <input type="text" placeholder="room" v-model="roomname" />
       <button v-on:click.prevent="addroom">submit</button>
-=======
-    <!-- <div id="nav"> -->
-    <!-- <input type="text" placeholder="room" v-model="roomname" />
-      <button v-on:click="addroom">submit</button>
->>>>>>> 8b290b3ef9bae6842a4c7343744bf67ab3ce5c96
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link to="/quiz">Quiz</router-link>
-    </div>-->
+    </div>
+     -->
     <router-view />
   </div>
 </template>
@@ -21,23 +16,22 @@ export default {
   name: 'app',
   data() {
     return {
-<<<<<<< HEAD
       roomname: null,
       copyname: null
-    };
+    }
   },
   methods: {
     addroom() {
       // this.$socket.emit("create-room", { name: this.roomname });
-      this.$socket.emit("game-start", this.roomname);
+      this.$socket.emit('game-start', this.roomname)
       // localStorage.setItem("roomname", this.roomname);
-      console.log(this.roomname);
-      let listenRoom = "datagame" + this.roomname;
-      console.log(listenRoom);
+      console.log(this.roomname)
+      let listenRoom = 'datagame' + this.roomname
+      console.log(listenRoom)
       this.$socket.on(listenRoom, payload => {
-        console.log(payload[0]);
-        console.log("datagame tambah roomname nih");
-      });
+        console.log(payload[0])
+        console.log('datagame tambah roomname nih')
+      })
     }
   },
   mounted() {
@@ -51,31 +45,6 @@ export default {
     // this.$socket.on("join-failed", data => {
     //   // console.log(data);
     // });
-=======
-      roomname: null
-    }
-  },
-  methods: {
-    addroom() {
-      let name = 'game-start'
-      this.$socket.emit(name, 'roomname')
-      this.$socket.emit('create-room', { name: this.roomname })
-    }
-  },
-  mounted() {
-    this.$socket.on('sample', data => {
-      console.log(data.message)
-    })
-
-    this.$socket.on('join-succes', data => {
-      console.log(data)
-    })
-    this.$socket.on('join-failed', data => {
-      console.log(data)
-    })
-
-    this.$socket.on('datagame' + 'roomname', payload => {})
->>>>>>> 8b290b3ef9bae6842a4c7343744bf67ab3ce5c96
   }
 }
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -8,36 +8,52 @@ const baseURL = 'http://localhost:3000'
 
 export default new Vuex.Store({
   state: {
-      question: '',
-      answer: '',
-
+    question: '',
+    answer: '',
+    allRooms: null
   },
   mutations: {
-      SET_QUESTION(state, payload) {
-        state.question = payload
-      },
-      SET_ANSWER(state, payload) {
-        state.answer = payload
-      },
-
+    SET_QUESTION(state, payload) {
+      state.question = payload
+    },
+    SET_ANSWER(state, payload) {
+      state.answer = payload
+    },
+    SET_ALLROOMS(state, payload) {
+      state.allRooms = payload
+    }
   },
   actions: {
-      getQuiz(context) {
-          axios({
-            url: `${baseURL}/quiz`,
-            method: 'GET',
-          })
-          .then(({ data }) => {
-            console.log(data)
-            context.commit('SET_QUESTION', data.results[0].question)
-            context.commit('SET_ANSWER', data.results[0].correct_answer)
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      },
+    getQuiz(context) {
+      axios({
+        url: `${baseURL}/quiz`,
+        method: 'GET'
+      })
+        .then(({ data }) => {
+          console.log(data)
+          context.commit('SET_QUESTION', data.results[0].question)
+          context.commit('SET_ANSWER', data.results[0].correct_answer)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getRooms(context) {
+      console.log('masuk sini')
 
+      axios({
+        url: `${baseURL}/rooms`,
+        method: 'GET'
+      })
+        .then(({ data }) => {
+          console.log(data, 'kasdbka')
+
+          context.commit('SET_ALLROOMS', data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
