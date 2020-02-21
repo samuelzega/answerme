903ed6e8125207d464b4bc75cb50bed11133f5a1
purandrar <andra_pur@yahoo.com>
2020-02-21 10:44:53 +0700

    bisa mengirim data quiz dari server ke client

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <input type="text" placeholder="room" v-model="roomname" />
-      <button v-on:click="addroom">submit</button>
+      <button v-on:click.prevent="addroom">submit</button>
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link to="/quiz">Quiz</router-link>
@@ -15,29 +15,35 @@ export default {
   name: "app",
   data() {
     return {
-      roomname: null
+      roomname: null,
+      copyname: null
     };
   },
   methods: {
     addroom() {
-      let name = "game-start";
-      this.$socket.emit(name, "roomname");
-      this.$socket.emit("create-room", { name: this.roomname });
+      // this.$socket.emit("create-room", { name: this.roomname });
+      this.$socket.emit("game-start", this.roomname);
+      // localStorage.setItem("roomname", this.roomname);
+      console.log(this.roomname);
+      let listenRoom = "datagame" + this.roomname;
+      console.log(listenRoom);
+      this.$socket.on(listenRoom, payload => {
+        console.log(payload[0]);
+        console.log("datagame tambah roomname nih");
+      });
     }
   },
   mounted() {
-    this.$socket.on("sample", data => {
-      console.log(data.message);
-    });
-
-    this.$socket.on("join-succes", data => {
-      console.log(data);
-    });
-    this.$socket.on("join-failed", data => {
-      console.log(data);
-    });
-
-    this.$socket.on("datagame" + "roomname", payload => {});
+    // this.$socket.on("sample", data => {
+    //   console.log(data.message);
+    //   this.copyname = data.roomname;
+    // });
+    // this.$socket.on("join-succes", data => {
+    //   console.log(data);
+    // });
+    // this.$socket.on("join-failed", data => {
+    //   // console.log(data);
+    // });
   }
 };
 </script>
--- server/controllers/apiquiz.js
+++ server/controllers/apiquiz.js
@@ -1,23 +1,30 @@
 const axios = require('axios')
 
 class ApiQuizController {
-    static apiquiz(req, res, next) {
-        axios({
-            url: 'https://opentdb.com/api.php',
-            method: 'GET',
-            params: {
-                amount: 1,
-                category: 11,
-                difficulty: 'easy',
-                type: 'boolean'
-            }
-        })
-        .then(({ data }) => {
-            res.status(200).json(data)
-        })
-        .catch(error => {
+    static async apiquiz() {
+        try {
+            return await axios({
+                url: 'https://opentdb.com/api.php',
+                method: 'GET',
+                params: {
+                    amount: 1,
+                    category: 11,
+                    difficulty: 'easy',
+                    type: 'boolean'
+                }
+            })
+            console.log(data, "ini data api");
+            // return data.results
+        } catch (error) {
             console.log(error)
-        })
+        }
+        // .then(({ data }) => {
+        //     console.log(data.results)
+        //     return data.results
+        // })
+        // .catch(error => {
+        //     console.log(`ini error`)
+        // })
     }
 }
 
